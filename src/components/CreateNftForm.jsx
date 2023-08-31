baac435d26dc7d2662bcf6eb6cf30532fca65424
Jhakejhosh <jhakejhosh83@gmail.com>
2023-08-31 10:40:58 +0100

    rectify

--- src/components/CreateNftForm.jsx
+++ src/components/CreateNftForm.jsx
@@ -2,9 +2,41 @@
 
 const CreateNftForm = () => {
 	return (
-		  <div>
-		    <h1>Create your own NFT here! 💡</h1>
-		    <form>Hello
+		  <div className="pt-24 px-4 dark:bg-darkBg dark:text-darkText">
+		    <h1 className="font-bold text-xl md:text-center">Create NFT💡</h1>
+		    <form className="py-6 w-full relative md:px-36">
+		      <div className="mb-6">
+		        <p className="font-semibold mb-2 text-sm">Name</p>
+		        <input type="text" placeholder="Type your item name here..." className="p-4 bg-gray-100 dark:bg-gray-900 rounded-md outline-0 text-gray-900 dark:text-gray-100 w-full" required/>
+		      </div>
+		      <div className="mb-6">
+		        <p className="font-semibold mb-2 text-sm">Add image cover</p>
+		        <input type="file" className="p-4 bg-gray-100 dark:bg-gray-900 rounded-md outline-0 text-gray-900 dark:text-gray-100 w-full" required/>
+		      </div>
+		      <div className="mb-6">
+		        <p className="font-semibold mb-2 text-sm">Description</p>
+		        <textArea placeholder="Say something about this item🚀" className="p-4 bg-gray-100 dark:bg-gray-900 rounded-md outline-0 text-gray-900 dark:text-gray-100 w-full" required/>
+		      </div>
+		      <div className="mb-6">
+		        <p className="font-semibold mb-2 text-sm">Choose item category</p>
+		        <select className="p-4 bg-gray-100 dark:bg-gray-900 rounded-md outline-0 text-gray-900 dark:text-gray-100 w-full" required>
+		          <option>Art</option>
+		          <option>Music</option>
+		          <option>Sport</option>
+		          <option>Game</option>
+		        </select>
+		      </div>  
+		        <div className="mb-6 flex justify-between items-center">
+		          <div>
+		            <p className="font-semibold mb-2 text-sm">High bid</p>
+		           <input type="number" placeholder="0.246" className="p-4 bg-gray-100 dark:bg-gray-900 rounded-md outline-0 text-gray-900 dark:text-gray-100 w-[150px]" required/>
+		          </div>
+		          <div>
+		            <p className="font-semibold mb-2 text-sm">Low bid</p>
+		           <input type="number" placeholder="0.123" className="p-4 bg-gray-100 dark:bg-gray-900 rounded-md outline-0 text-gray-900 dark:text-gray-100 w-[150px]" required/>
+		          </div>
+		        </div>
+		        <button type="submit" className="p-4 rounded-md text-sm font-bold bg-subColor text-darkText w-full">Create</button>
 		    </form>
 		  </div>
 		)
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -12,8 +12,10 @@ const Signup = () => {
 		      <h1 className="text-xl font-bold mb-2">Sign Up Here!</h1>
 		      <p className="text-gray-400 font-medium px-8">Please enter your account details here</p>
 		      <form className="my-4 w-full md:w-[50%]">
+		        <div><input type="file" className="border-2 border-gray-400 rounded-md w-full p-2 my-4" required/></div>
+		        <div><input type="Text" placeholder="Username" className="border-2 border-gray-400 rounded-md w-full p-2 my-4" required/></div>
 		        <div><input type="Email" placeholder="email" className="border-2 border-gray-400 rounded-md w-full p-2 my-4" required/></div>
-		        <div><input type="Password" placeholder="password" className="border-2 border-gray-400 rounded-md w-full p-2 mb-6"required/></div>
+		        <div><input type="Password" placeholder="password" className="border-2 border-gray-400 rounded-md w-full p-2 my-4"required/></div>
 		        <button type="submit" className="border-0 outline-0 rounded-md w-full p-2 bg-subColor text-darkText font-semibold">Sign up</button>
 		      </form>
 		      <p className="text-gray-400">Have an account already? <Link to="/login" className="text-subColor font-semibold">Log in</Link></p>
